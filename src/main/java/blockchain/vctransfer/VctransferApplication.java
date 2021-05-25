cba7ae4b70a041b19e8205a36c5ac1f7c850a53f
Ahmed <ahmedbasuny13@gmail.com>
2021-05-25 12:20:43 +0200

    initial commit

---
+++ src/main/java/blockchain/vctransfer/VctransferApplication.java
@@ -0,0 +1,13 @@
+package blockchain.vctransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VctransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VctransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blockchain/vctransfer/VctransferApplicationTests.java
@@ -0,0 +1,13 @@
+package blockchain.vctransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VctransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
